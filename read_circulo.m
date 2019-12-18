7d8316f49f33d385b1a41acdb3c4e84e7c6b5b17
Antonio <asanchez@isa.upv.es>
2019-12-18 09:05:51 +0100

    Revert "v0"
    This reverts commit 88d1d652c9cd38b79d1b90960f43a32fa3293cb3.

--- read_circulo.m
+++
@@ -1,8 +0,0 @@
-function imagenComprimida = read_circulo(filename)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-    imageID = fopen(filename);
-    imagenComprimida = fread(imageID,'uint8');
-    fclose(imageID);
-end
-
--- read_img.m
+++
@@ -1,5 +0,0 @@
-function img = read_img(filename, width, height)
-    imagenComprimida = read_circulo(filename);
-    img = descomprimirGray(imagenComprimida, width, height);
-end
-
